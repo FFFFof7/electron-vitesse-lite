6f613589280d3c4cc1331577c2195a3f6b0331d9
FFFFof <fhm011101@126.com>
2022-04-28 16:07:11 +0800

    chore:init

---
+++ packages/renderer/vite.config.ts
@@ -0,0 +1,71 @@
+/// <reference types="vitest" />
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
+import resolve from 'vite-plugin-resolve'
+import electron from 'vite-plugin-electron/renderer'
+import pkg from '../../package.json'
+
+
+export default defineConfig({
+  root: __dirname,
+  base: './',
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue({
+      reactivityTransform: true,
+    }),
+    electron(),
+    resolve(
+      /**
+       * Here you can specify other modules
+       * 🚧 You have to make sure that your module is in `dependencies` and not in the` devDependencies`,
+       *    which will ensure that the electron-builder can package it correctly
+       */
+      {
+        // If you use electron-store, this will work - ESM format code snippets
+        'electron-store': 'const Store = require("electron-store"); export default Store;',
+      }
+    ),
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+        '@vueuse/core',
+      ],
+      dts: true,
+    }),
+
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+  ],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    environment: 'jsdom',
+  },
+  server: {
+    // host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
+  },
+})
