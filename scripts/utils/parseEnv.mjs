e12381fcd524e30e867058f030a71808cea932d6
FFFFof <fhm011101@126.com>
2022-05-06 16:43:36 +0800

    edit env

--- scripts/utils/parseEnv.mjs
+++ scripts/utils/parseEnv.mjs
@@ -2,5 +2,5 @@ import path from 'path'
 import { loadEnv } from 'vite'
 export default function parseEnv(mode) {
   const envDir = path.resolve(process.cwd(), './env')
-  return loadEnv(mode, envDir, [''])
+  return loadEnv(mode, envDir, '')
 }
