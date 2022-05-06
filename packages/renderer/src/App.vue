55671c75b7cad7392d64b338e5189992e3f9c72a
FFFFof <fhm011101@126.com>
2022-05-06 15:24:40 +0800

    typecheck

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,6 +1,7 @@
+<script></script>
 <template>
   <!-- <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200"> -->
-    <router-view />
-    <!-- <Footer /> -->
+  <router-view />
+  <!-- <Footer /> -->
   <!-- </main> -->
 </template>
