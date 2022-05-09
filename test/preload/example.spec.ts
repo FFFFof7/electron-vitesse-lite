d8f4e6ffcaab24f675234947720a980c43771fd1
FFFFof <fhm011101@126.com>
2022-05-09 14:50:27 +0800

    del test

--- test/preload/example.spec.ts
+++ test/preload/example.spec.ts
@@ -1,11 +1,6 @@
 import { describe, expect, test } from 'vitest'
-import { estT } from '../../packages/preload/utils'
 describe('preload test should be defined', () => {
   test('1 plus 1 equals 2', () => {
     expect(1 + 1).toBe(2)
-  })
-  test('estT', () => {
-    const str = '1'
-    expect(estT(str)).toBe('2')
   })
 })
