c7afcc73b070d269c2a55b52fdee47402d9f52f8
FFFFof <fhm011101@126.com>
2022-05-09 16:40:31 +0800

    builder

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -5,13 +5,14 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
-import resolve from 'vite-plugin-resolve'
-import electron from 'vite-plugin-electron/renderer'
+// import resolve from 'vite-plugin-resolve'
+// import electron from 'vite-plugin-electron/renderer'
 
 export default defineConfig(({ mode }) => {
   const envDir = path.resolve(__dirname, '../../env')
   const { VITE_DEV_SERVER_PORT } = loadEnv(mode, envDir)
   return {
+    base: './',
     root: __dirname,
     envDir,
     resolve: {
@@ -23,18 +24,18 @@ export default defineConfig(({ mode }) => {
       Vue({
         reactivityTransform: true,
       }),
-      electron(),
-      resolve(
-        /**
-         * Here you can specify other modules
-         * 🚧 You have to make sure that your module is in `dependencies` and not in the` devDependencies`,
-         *    which will ensure that the electron-builder can package it correctly
-         */
-        {
-          // If you use electron-store, this will work - ESM format code snippets
-          'electron-store': 'const Store = require("electron-store"); export default Store;',
-        },
-      ),
+      // electron(),
+      // resolve(
+      //   /**
+      //    * Here you can specify other modules
+      //    * 🚧 You have to make sure that your module is in `dependencies` and not in the` devDependencies`,
+      //    *    which will ensure that the electron-builder can package it correctly
+      //    */
+      //   {
+      //     // If you use electron-store, this will work - ESM format code snippets
+      //     'electron-store': 'const Store = require("electron-store"); export default Store;',
+      //   },
+      // ),
       // https://github.com/hannoeru/vite-plugin-pages
       Pages(),
 
@@ -60,6 +61,14 @@ export default defineConfig(({ mode }) => {
         configFile: path.resolve(__dirname, './unocss.config.ts'),
       }),
     ],
+    build: {
+      outDir: '../../dist/renderer',
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        external: ['election'],
+      },
+    },
 
     // https://github.com/vitest-dev/vitest
     test: {
